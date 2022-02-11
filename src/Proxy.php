a732fd652a1ca06fed1746f489feba7c917acc68
Jan Barášek <janbarasek@gmail.com>
2022-02-11 22:23:31 +0100

    Proxy: Implement logic for download and manage external images.

---
+++ src/Proxy.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Baraja\ImageGenerator;
+
+
+use Nette\Utils\FileSystem;
+
+final class Proxy
+{
+	private static ?string $basePath = null;
+
+
+	public static function save(string $url, string $hash): void
+	{
+		$path = self::getStoragePath($hash, pathinfo($url)['extension']);
+		if (is_file($path)) {
+			return;
+		}
+		FileSystem::write($path, FileSystem::read($url));
+	}
+
+
+	public static function getStoragePath(?string $hash = null, ?string $extension = null): string
+	{
+		$return = self::getBasePath();
+		if ($hash !== null) {
+			$return = sprintf('%s/%s/%s', $return, substr($hash, 0, 3), $hash);
+			if ($extension !== null) {
+				$return .= sprintf('.%s', $extension);
+			}
+		}
+
+		return $return;
+	}
+
+
+	public static function getBasePath(): string
+	{
+		if (self::$basePath === null) {
+			self::$basePath = sprintf('%s/_cache/_proxy', Helper::getWwwDir());
+		}
+
+		return self::$basePath;
+	}
+}
